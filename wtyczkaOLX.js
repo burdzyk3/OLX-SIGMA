f6c695b019e047cdd9d0db5517367a20d432f482
Wiktor <dsw51733@student.dsw.edu.pl>
2025-04-30 19:19:07 +0200

    Adds downloading ids from site and sorting if id is reapeted

--- wtyczkaOLX.js
+++ wtyczkaOLX.js
@@ -1,11 +1,34 @@
-const carts = document.querySelectorAll('[data-cy="l-card"]');
+const seenIds = new Set();
 
 let i = 0;
 
-carts.forEach((cart) => {
-	const id = cart.id;
-	i += 1;
-	console.log("ogloszenie nr:", i, "ID =", id);
+function olxReapetedCarts() {
+	const carts = document.querySelectorAll('[data-cy="l-card"]');
+	carts.forEach((cart) => {
+		const id = cart.id;
+		i += 1;
+		// console.log("ogloszenie nr:", i, "ID =", id);
 
-    
+		if (seenIds.has(id)) {
+			console.log("Ukryto powtarzajace sie ogloszenie z ID: ", id);
+		} else {
+			seenIds.add(id);
+			console.log("Dodano nowe ID: ", id);
+		}
+	});
+}
+
+const observer = new MutationObserver((mutationsList, observer) => {
+	for (const mutation of mutationsList) {
+		if (mutation.addedNodes.length > 0) {
+			olxReapetedCarts();
+		}
+	}
 });
+
+observer.observe(document.body, {
+	childList: true,
+	subtree: true,
+});
+
+olxReapetedCarts();
